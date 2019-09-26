f16f39476471f1b8cf652cbc78911681650105d7
Felipe Soares Barbosa Silveira <felipeiturama@gmail.com>
2019-09-26 17:49:44 -0300

    Primeiro commit

---
+++ duthread.h
@@ -0,0 +1,19 @@
+#ifndef DUTHREAD_H
+#define DUTHREAD_H
+
+#include <QThread>
+
+class DuThread : public QThread
+{
+    Q_OBJECT
+public:
+    DuThread(int tiempo, QObject *parent = nullptr);
+signals:
+    void valorCambiado(int);
+protected:
+    void run() override;
+private:
+    int mTiempo; // milisegundos
+};
+
+#endif // DUTHREAD_H
